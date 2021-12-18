307107ba6b2bb7dc3a5bbd16b7dd62c99905f5b9
Valeria Makarevich <valeriamakarevich@outlook.com>
2021-12-18 12:59:30 +0300

    Выполнила задание 7

---
+++ Задание_2.7/Задание_2.7.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const float pi = 3.14;
+    float a=-0.4*pi, b= 0.4*pi, h=0.5;
+    while (a<=b) {
+        std::cout << "y("<< a <<"): " << pow(a, 2) * sqrt(15 + 10 * sin(a + pi)) << std::endl;
+        a += h;
+    }
+    return 0;
+}
+
