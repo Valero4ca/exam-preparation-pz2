bc11f7393cd8459a89ec2439ac41ee95b63d27ac
Valeria Makarevich <valeriamakarevich@outlook.com>
2021-12-23 20:51:07 +0300

    Сделала задание 20

---
+++ Задание_2.20/Задание_2.20.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double  fraction, integer;
+	int add=0;
+	double number;
+	std::cin >> number;
+	while (number > 0) {
+		number/= 10;
+		fraction = modf(number, &integer); 
+		fraction *= 10;
+		if (std::lround(fraction) == 2) {
+			add++;
+		}
+		number = integer;
+
+	}
+	std::cout << "Количество двоек в числе : "<< add;
+}
+
